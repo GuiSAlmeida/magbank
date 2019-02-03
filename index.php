c31ebf94d6463a5241e75b94730f4749f0734b05
Gui Almeida <ga.designer.art@gmail.com>
2019-02-03 00:07:06 -0200

    inicializando repositorio

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php include("header.php") ?>
+    
+<header class="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="assets/images/logo-white.png" alt="Magbank">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Cartão</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quem somos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+                </ul>
+                
+                <div class="btn-group">
+                    <button type="button" class="btn btn-outline-light">Acessar minha conta</button>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-outline-light dropdown-toggle" data-toggle="dropdown">
+                        abra sua conta
+                        </button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Pessoa física</a>
+                            <a class="dropdown-item" href="#">Pessoa jurídica</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </nav>
+</header>
+
+<section class="banner">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/images/banner-desktop.jpg" class="banner-desktop w-100">
+                <img src="assets/images/banner-mobile.jpg" class="banner-mobile w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/banner-desktop.jpg" class="banner-desktop w-100">
+                <img src="assets/images/banner-mobile.jpg" class="banner-mobile w-100">
+            </div>
+        </div>
+
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    </div>
+
+</section>
+
+<section class="credit-card py-5">
+    <div class="container">
+        <div class="row align-items-md-center">
+            <div class="col-12 col-md-6 col-lg-7">
+                <h2 class="display-1 text-primary mb-4">Cartão de crédito</h2>
+
+                <ul class="credit-card-list mb-md-5">
+                    <li><i class="fas fa-credit-card"></i>Crédito pessoal</li>
+                    <li><i class="fas fa-mobile"></i>APP</li>
+                    <li><i class="fas fa-shopping-cart"></i>Pagamento online</li>
+                    <li><i class="fas fa-wallet"></i>Carteira digital</li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-lg-5 text-center">
+                <img class="img-fluid my-5 mt-md-0" src="assets/images/card.png">
+            </div>
+        </div>
+        
+        <section class="cards py-5">
+            <div class="container">
+                <div class="row d-flex justify-content-md-between">
+                    <div class="col-12 col-md-4">
+                        <div class="card mb-3">
+                            <img src="assets/images/card1.jpg" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Lorem ipsum dolor sit</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue.</p>
+                                <a href="#" class="btn btn-danger">Peça já</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-4">
+                        <div class="card mb-3">
+                            <img src="assets/images/card2.jpg" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Lorem ipsum dolor sit</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue.</p>
+                                <a href="#" class="btn btn-danger">Peça já</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-4">
+                        <div class="card">
+                            <img src="assets/images/card3.jpg" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Lorem ipsum dolor sit</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue.</p>
+                                <a href="#" class="btn btn-danger">Peça já</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-center mt-5">
+                <button href="#" type="button" class="btn btn-success btn-lg px-4">Abra sua conta</button>
+            </div>
+        </section>
+    </div>
+</section>
+
+<section class="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-7">
+                <div class="d-flex justify-content-md-end">
+                    <div class="text-light">
+                        <h2 class="display-1">Já nascemos digital</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.</p>
+
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include("footer.php") ?>
